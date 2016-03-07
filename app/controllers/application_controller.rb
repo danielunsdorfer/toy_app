996d6e4b285f4fe805af3f7845d44b8f46ee448f
Daniel Unsdorfer <daniel.unsdorfer@gmail.com>
2016-03-07 21:44:44 +0000

    Adds hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def hello
+    render text: "I built this shitty web site so I could show off to fools like you"
+  end
+  
 end
