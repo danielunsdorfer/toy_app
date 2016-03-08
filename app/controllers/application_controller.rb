8b0f883cb740110c1ec1dc5c4a7aab588e8ec232
Daniel Unsdorfer <daniel.unsdorfer@gmail.com>
2016-03-08 22:06:14 +0000

    Finishes toy app exercise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  def hello
-    render text: "I built this shitty web site so I could show off to fools like you"
-  end
+  #def hello
+  #  render text: "I built this shitty web site so I could show off to fools like you"
+  #end
   
 end
